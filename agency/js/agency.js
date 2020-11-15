4c6b58edd7c9fa715c0f664c9485e9824232d5cd
webpages-hub <63662796+webpages-hub@users.noreply.github.com>
2020-11-15 09:35:02 +0100

    Add files via upload

---
+++ agency/js/agency.js
@@ -0,0 +1,6 @@
+/*var images = ['img/female2.jpeg' , 'img/female3.jpeg' , 'img/female4.jpeg' , 'img/female5.jpeg' ]
+var imageHead = document.getElementsByClassName('section');
+var i = 0;
+setInterval (function() {
+	document.getElementsByClassName('section').style.backgroundImage= "url(" + images[i] +")";
+}, 1000);*/
